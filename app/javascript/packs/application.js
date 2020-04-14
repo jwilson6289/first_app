27c77b0309220d1dddb856400ac18c5397e57358
jwilson6289 <joshwilsonsound@gmail.com>
2020-04-14 18:48:24 -0500

    Hopefully added Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+<%= stylesheet_pack_tag 'application', media: 'all' %>
+<%= javascript_pack_tag 'application' %>
+import 'css/stylesheet'
 
+import 'jquery'
+import 'popper.js'
+import 'bootstrap'
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
