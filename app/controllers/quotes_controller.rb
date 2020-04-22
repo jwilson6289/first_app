3e87e118fc39a38d225d9b01b0e3dc3dbf027693
jwilson6289 <joshwilsonsound@gmail.com>
2020-04-22 17:54:09 -0500

    Save data on submit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,8 +2,20 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("Random()").first
   end
-end
 
-def new
-  @quote = Quote.new
+  def new
+    @quote = Quote.new
+  end
+
+  def create
+    Quote.create(quote_params)
+    redirect_to root_path
+  end
+
+
+private
+
+  def quote_params
+    params.require(:quote).permit(:saying, :author)
+  end
 end
